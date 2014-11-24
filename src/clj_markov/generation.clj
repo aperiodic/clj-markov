5d094bf91a58d3339ff8ad35c63523f2420532c4
aperiodic <dlp@aperiodic.org>
2014-11-23 22:34:03 -0800

    Refactor token generation.
    Break out the generation of a single new output token into a function
    that can reduced (kinda).
    
    Generate a fixed number of output tokens by reducing the above function
    over (range amount).

--- src/clj_markov/generation.clj
+++ src/clj_markov/generation.clj
@@ -1,21 +1,28 @@
 (ns clj-markov.generation
   (:require [clj-markov.training :refer [shift-and-append]]))
 
+(defn generate*
+  [{:keys [state output]} chain]
+  (let [antecedents (get chain state)
+        weight-sum (reduce + (vals antecedents))
+        selection (rand weight-sum)
+        [_ next-token] (reduce (fn [[position selected-token] [token weight]]
+                                 (if selected-token
+                                   [-1 selected-token]
+                                   (let [position' (+ position weight)]
+                                     (if (> position' selection)
+                                       [-1 token]
+                                       [position' nil]))))
+                               [0 nil]
+                               (seq antecedents))]
+    {:state (shift-and-append state next-token)
+     :output (conj output (name next-token))}))
+
 (defn generate
-  [chain amount]
-  (loop [state (rand-nth (keys (dissoc chain :clj-markov.training/state))), out []]
-    (if (= (count out) amount)
-      out
-      (let [antecedents (get chain state)
-            weight-sum (reduce + (vals antecedents))
-            selection (rand weight-sum)
-            [_ next-token] (reduce (fn [[position selected-token] [token weight]]
-                                     (if selected-token
-                                       [-1 selected-token]
-                                       (let [position' (+ position weight)]
-                                         (if (> position' selection)
-                                           [-1 token]
-                                           [position' nil]))))
-                                   [0 nil]
-                                   (seq antecedents))]
-        (recur (shift-and-append state next-token), (conj out (name next-token)))))))
+  ([chain amount]
+   (let [s0 (rand-nth (keys (dissoc chain :clj-markov.training/state)))]
+     (generate chain s0 amount)))
+  ([chain starting-state amount]
+   (:output (reduce (fn [sm _] (generate* sm chain))
+                    {:state starting-state, :output []}
+                    (range amount)))))
